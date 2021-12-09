75109554647e20b2f9fb2a89216efa1a19b1a621
Abi Sidford <asidford@gmail.com>
2021-12-09 11:59:50 +0000

    Added home endpoint and test

---
+++ src/main/java/com/siddy/placestogo/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.siddy.placestogo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping(value = "/")
+    public String home(){
+        return "Welcome to the Events API!";
+    }
+}
