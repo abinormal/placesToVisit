a1da6b4a729008537c2fa7d044f1d6e6637e0a3f
Abi Sidford <asidford@gmail.com>
2021-12-09 11:48:20 +0000

    First commit - added Spring Boot

---
+++ src/main/java/com/siddy/placestogo/PlacestogoApplication.java
@@ -0,0 +1,13 @@
+package com.siddy.placestogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacestogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacestogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siddy/placestogo/PlacestogoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddy.placestogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacestogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
